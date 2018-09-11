c0b3d437fd26fc282cab8b65fa5f0187228b6ec5
qiangshushu <597515427@qq.com>
2018-09-11 10:16:49 +0800

    qss

--- src/views/bespeak/components/list.vue
+++ src/views/bespeak/components/list.vue
@@ -51,82 +51,81 @@
 
 <style lang="less" scoped>
   .appointment-list {
-      position: absolute;
-      left: 0px;
-      top: 112px;
-      bottom: 0;
-      right: 0px;
-      background-color: #f0f0f0;
-      overflow: auto;
-      box-shadow: 0 10px 50px 10px rgba(170, 170, 170, 0.5);
-      .appointment {
-          margin: 20px 20px 16px;
-          border-radius: 5px;
-          background-color: #fff;
-          &-header {
-              display: flex;
-              justify-content: space-around;
-              align-items: center;
-              vertical-align: middle;
-              height: 64px;
-              border-bottom: 1px solid #f0f0f0; /*no*/
-              padding: 0 20px;
+    position: absolute;
+    left: 0px;
+    top: 96px;
+    bottom: 0;
+    right: 0px;
+    background-color: #f0f0f0;
+    overflow: auto;
+    .appointment {
+      margin: 20px 20px 16px;
+      border-radius: 5px;
+      background-color: #fff;
+      &-header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        vertical-align: middle;
+        height: 64px;
+        border-bottom: 1px solid #f0f0f0; /*no*/
+        padding: 0 20px;
 
-              .name {
-                  width: 60%;
-                  font-size: 24px;
-                  img {
-                      margin-right: 12px;
-                      margin-top: -2px;
-                      vertical-align: middle;
-                      width: 24px;
-                      height: 24px;
-                  }
-              }
-              .address {
-                  width: 40%;
-                  text-align: right;
-              }
+        .name {
+          width: 60%;
+          font-size: 24px;
+          img {
+            margin-right: 12px;
+            margin-top: -2px;
+            vertical-align: middle;
+            width: 24px;
+            height: 24px;
           }
-          &-conetnt {
-              padding: 0px 20px;
-              border-bottom: 1px solid #f0f0f0; /*no*/
-              .time {
-                  margin-top: 30px;
-              }
-              .user {
-                  margin-top: 18px;
-              }
-              .time,
-              .user,
-              .detail {
-                  font-size: 24px;
-                  color: #666666;
-              }
-              .detail {
-                  color: #999999;
-                  width: 660px;
-                  margin: 30px 0;
-                  overflow: hidden;
-                  text-overflow: ellipsis;
-                  white-space: nowrap;
-              }
-          }
-          &-footer {
-              height: 80px;
-              padding: 0 20px;
-              .btn {
-                  float: right;
-                  width: 140px;
-                  text-align: center;
-                  line-height: 52px;
-                  line-height: 52px;
-                  border-radius: 26px;
-                  border: 1px solid #dcdcdc; /*no*/
-                  margin-top: 15px;
-                  margin-left: 30px;
-              }
-          }
+        }
+        .address {
+          width: 40%;
+          text-align: right;
+        }
       }
+      &-conetnt {
+        padding: 0px 20px;
+        border-bottom: 1px solid #f0f0f0; /*no*/
+        .time {
+          margin-top: 30px;
+        }
+        .user {
+          margin-top: 18px;
+        }
+        .time,
+        .user,
+        .detail {
+          font-size: 24px;
+          color: #666666;
+        }
+        .detail {
+          color: #999999;
+          width: 660px;
+          margin: 30px 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+      }
+      &-footer {
+        height: 80px;
+        padding: 0 20px;
+        .btn {
+          float: right;
+          width: 140px;
+          text-align: center;
+          line-height: 52px;
+          line-height: 52px;
+          border-radius: 26px;
+          border: 1px solid #dcdcdc; /*no*/
+          margin-top: 15px;
+          margin-left: 30px;
+        }
+      }
+    }
   }
 </style>
--- src/views/cancelList.vue
+++ src/views/cancelList.vue
@@ -12,7 +12,7 @@
         </div>
         <div class="itemcontent">
           <div class="contentleft">
-            <img src="~assets/mypage/icon_order_5.png" alt="">
+            <img src="~assets/goods/pic_wring1.png" alt="">
           </div>
           <div class="contentright">
             <div class="contenttitle">
@@ -30,7 +30,7 @@
         </div>
         <div class="itemcontent">
           <div class="contentleft">
-            <img src="~assets/mypage/icon_order_5.png" alt="">
+            <img src="~assets/goods/pic_wring1.png" alt="">
           </div>
           <div class="contentright">
             <div class="contenttitle">
@@ -86,118 +86,118 @@
 </script>
 <style lang="less" scoped>
   .listitem {
-      background: #ffffff;
-      margin-bottom: 16px;
-      .itemtitle {
-          padding: 0 30px;
-          height: 64px;
-          line-height: 64px;
-          margin-top: 20px;
+    background: #ffffff;
+    margin-bottom: 16px;
+    .itemtitle {
+      padding: 0 30px;
+      height: 64px;
+      line-height: 64px;
+      margin-top: 20px;
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      .titleleft {
+        img {
+          display: inline;
+          width: 24px;
+          height: 24px;
+          margin-right: 12px;
+        }
+      }
+      .listright {
+        font-size: 24px;
+        color: #cdb498;
+      }
+    }
+    .itemcontent {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      padding: 0 30px;
+      background: #f5f5f5;
+      margin-bottom: 8px;
+      .contentleft {
+        width: 120px;
+        height: 120px;
+        margin: 20px 20px 20px 0;
+        background: #ffffff;
+        img {
+          display: block;
+          width: 100%;
+          height: auto;
+        }
+      }
+      .contentright {
+        flex: 1;
+        .contenttitle {
           display: flex;
           flex-flow: row nowrap;
           justify-content: space-between;
-          .titleleft {
-              img {
-                  display: inline;
-                  width: 24px;
-                  height: 24px;
-                  margin-right: 12px;
-              }
-          }
-          .listright {
-              font-size: 24px;
-              color: #cdb498;
-          }
-      }
-      .itemcontent {
-          display: flex;
-          flex-flow: row nowrap;
-          justify-content: space-between;
-          padding: 0 30px;
-          background: #f5f5f5;
-          margin-bottom: 8px;
-          .contentleft {
-              width: 120px;
-              height: 120px;
-              margin: 20px 20px 20px 0;
-              background: #ffffff;
-              img {
-                  display: block;
-                  width: 100%;
-                  height: auto;
-              }
-          }
-          .contentright {
-              flex: 1;
-              .contenttitle {
-                  display: flex;
-                  flex-flow: row nowrap;
-                  justify-content: space-between;
-                  margin: 30px 0 20px;
-                  color: #666666;
-                  font-size: 24px;
-                  span:nth-child-of(2) {
-                      text-align: right;
-                  }
-              }
-              .contentmessage {
-                  flex: 1;
-                  display: flex;
-                  flex-flow: row nowrap;
-                  justify-content: space-between;
-                  font-size: 24px;
-                  margin-bottom: 30px;
-                  color: #999999;
-                  p {
-                      width: 360px;
-                  }
-                  .messageright {
-                      text-align: right;
-                  }
-                  s {
-                      color: #cccccc;
-                      display: block;
-                  }
-              }
-          }
-      }
-      .itemprice {
-          height: 64px;
-          line-height: 64px;
-          text-align: right;
-          padding: 0 30px;
-          font-size: 20px;
+          margin: 30px 0 20px;
           color: #666666;
-          border-bottom: 2px solid #cccccc;
-          span {
-              font-size: 24px;
+          font-size: 24px;
+          span:nth-child-of(2) {
+            text-align: right;
           }
+        }
+        .contentmessage {
+          flex: 1;
+          display: flex;
+          flex-flow: row nowrap;
+          justify-content: space-between;
+          font-size: 24px;
+          margin-bottom: 30px;
+          color: #999999;
+          p {
+            width: 360px;
+          }
+          .messageright {
+            text-align: right;
+          }
+          s {
+            color: #cccccc;
+            display: block;
+          }
+        }
       }
-      .itemfoot {
-          height: 80px;
-          text-align: right;
-          padding: 0 30px;
-          button {
-              width: 140px;
-              height: 52px;
-              border-radius: 25px;
-              font-size: 20px;
-              margin-top: 14px;
-          }
-          .btnleft {
-              margin-right: 30px;
-          }
-          .btngrey {
-              background: #ffffff;
-              border: 2px solid #cccccc;
-              color: #666666;
-          }
-          .btnpink {
-              background: #ffb4b4;
-              border: 2px solid #ffb4b4;
-              color: #ffffff;
-          }
+    }
+    .itemprice {
+      height: 64px;
+      line-height: 64px;
+      text-align: right;
+      padding: 0 30px;
+      font-size: 20px;
+      color: #666666;
+      border-bottom: 2px solid #cccccc;
+      span {
+        font-size: 24px;
       }
+    }
+    .itemfoot {
+      height: 80px;
+      text-align: right;
+      padding: 0 30px;
+      button {
+        width: 140px;
+        height: 52px;
+        border-radius: 25px;
+        font-size: 20px;
+        margin-top: 14px;
+      }
+      .btnleft {
+        margin-right: 30px;
+      }
+      .btngrey {
+        background: #ffffff;
+        border: 2px solid #cccccc;
+        color: #666666;
+      }
+      .btnpink {
+        background: #ffb4b4;
+        border: 2px solid #ffb4b4;
+        color: #ffffff;
+      }
+    }
   }
 </style>
 
