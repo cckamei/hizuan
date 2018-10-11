ed7600b79fad4279b6f40831203e0517eaab46fb
qiangshushu <597515427@qq.com>
2018-10-11 18:13:08 +0800

    qss

--- src/views/goodsdetail.vue
+++ src/views/goodsdetail.vue
@@ -508,7 +508,7 @@
             num: this.sku.count,
             kezi: this.lettering.text,
             yaoqiu: this.lettering.remarks,
-            emp_id: '' //TODO
+            emp_id: this.getUserInfo.is_distributor || ''
           }
         }).then(res => {
           this.toast('已加入购物车');
